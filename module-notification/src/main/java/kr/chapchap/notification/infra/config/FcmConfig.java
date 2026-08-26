6afd2274bf7af2071f84af7767cdcfaa55efb656
kayoung <pgy8404@naver.com>
2026-08-26 20:25:01 +0900

    fix: fcmconfig test mock값 추가 및 우회

--- module-notification/src/main/java/kr/chapchap/notification/infra/config/FcmConfig.java
+++ module-notification/src/main/java/kr/chapchap/notification/infra/config/FcmConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+
+@ConditionalOnProperty(prefix = "chapchap.notification.fcm", name = "project-id")
 @EnableConfigurationProperties(FcmProperties.class)
 @Configuration
 public class FcmConfig {
